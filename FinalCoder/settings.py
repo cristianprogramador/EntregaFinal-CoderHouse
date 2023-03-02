a70f3e3412d36927dd2736591bbd2a9bfcf826db
Cristian Cerda <cristiandrivedatos@gmail.com>
2023-03-02 00:36:12 -0300

    guarda02/03/2023

--- FinalCoder/settings.py
+++ FinalCoder/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #ruta instalada por mi
+    'AppCoder.apps.AppcoderConfig',
 ]
 
 MIDDLEWARE = [
