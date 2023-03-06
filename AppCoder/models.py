63906307f8de70c1eecdafed86ff6e79ecc24544
Cristian Cerda <cristiandrivedatos@gmail.com>
2023-03-06 14:20:55 -0300

    guarda06/03/2023

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,3 +1,14 @@
+from pickle import TRUE
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+
+    def __str__(self) :
+        return f"{self.titulo}"
+
+    titulo= models.CharField(max_length=30)
+    descripcion= models.CharField(max_length=70)
+    genero= models.CharField(max_length=30)
+    fecha_de_estreno= models.DateField()
+    tipo_de_juego= models.CharField(max_length=60)
+    caratula= models.ImageField(upload_to = 'juegos', null=TRUE, blank=TRUE)
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,23 +1,163 @@
 from django.shortcuts import render
+from AppCoder.forms import *
+from AppCoder.models import *
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin 
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
+def registro(request):
+    if request.method == "POST":
+
+        form = RegistroUsuario(request.POST)
+
+        if form.is_valid():
+
+            username= form.cleaned_data["username"]
+
+            form.save()
+            
+            return render (request,"AppCoder/resultadoregistro.html", {"mensaje": "Usuario creado."}) 
+
+    else: 
+
+        form = RegistroUsuario()
+
+    return render (request,"AppCoder/registro.html", {"formulario":form})
+
+def resultadoregistro(request):
+    return render (request,"AppCoder/resultadoregistro.html")
+
+def iniciosesion(request):
+    if request.method=="POST":
+
+        form= AuthenticationForm(request, data= request.POST)
+
+        if form.is_valid():
+
+            usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+
+            user = authenticate(username = usuario, password= contraseña)
+
+            if user:
+
+                login(request,user)
+
+                return render (request,"AppCoder/inicio.html", {"usuario": user})
+            
+        else:
+            
+            form = AuthenticationForm()
+            return render (request,"AppCoder/iniciosesion.html", {"mensaje": "Datos incorrectos. Intente nuevamente.", "formulario": form}) 
+
+    else:
+
+        form = AuthenticationForm()
+
+    return render (request,"AppCoder/iniciosesion.html", {"formulario": form})
+
+#+++++++++++++++++++++++++++++++++
 def inicio(request):
     return render(request, 'AppCoder/inicio.html')
 
-#def buscar(request):
-    if request.GET['camada']:
-        camada = request.GET['camada']
-        cursos = Curso.objects.filter(camada__icontains = camada)
-        return render(request, 'AppCoder/inicio.html', {'cursos': cursos, 'camada': camada})
+    
+def about(request):
+    return render (request,"AppCoder/about.html")
 
+#++++++++++++++++++++++++++++++++++++++
+@login_required
+def editarperfil(request):
+    usuario= request.user
+
+    if request.method == "POST":
+
+        form= FormularioEditar(request.POST)
+    
+        if form.is_valid():
+
+            info = form.cleaned_data
+
+            usuario.username= info["username"]
+            usuario.email= info ["email"]
+            usuario.set_password(info["password1"])
+
+            usuario.save()
+
+
+        return render (request, "AppCoder/inicio.html")
+        
     else: 
-        respuesta = 'ERROR CAMPO SIN RELLENO'
+        
+        form= FormularioEditar(initial={
+            "email": usuario.email, 
+            })
+        
+        contexto=  {"formulario": form, "usuario": usuario}
 
-    return render(request, 'AppCoder/inicio.html', {"respuesta": respuesta})
+    return render (request, "AppCoder/editarperfil.html", contexto)
+    
 
-def iniciosesion(request):
-    return render(request, 'AppCoder/iniciosesion.html')
 
-def registro(request):
-    return render(request, 'AppCoder/registro.html')
+def buscar(request):
+    return render (request,"AppCoder/busquedaGames.html")
+
+def busquedaGame(request):
+
+
+    if "tit" in request.GET:
+
+        titulo = request.GET["tit"]
+
+    else:
+
+        titulo = False
+
+
+    if titulo:
+     
+        titulo= Game.objects.filter(titulo__icontains=titulo)
+
+        return render (request,"AppCoder/busquedaGames.html", {"titulo": titulo})
+
+    else:
+
+        return render(request, "AppCoder/busquedaGames.html")
+
+
+class ListaJuego(LoginRequiredMixin, ListView):
+    
+    model= Game 
+
+
+class DetalleJuego(LoginRequiredMixin, DetailView):
+
+    model= Game
+
+    
+class CrearJuego(LoginRequiredMixin, CreateView):
+
+    model= Game
+    success_url = "/juego/list"
+    fields = ["titulo", "descripcion", "genero", "fecha_de_estreno", "tipo_de_juego", "caratula"]
+
+      
+class ActualizarJuego(LoginRequiredMixin, UpdateView):
+
+    model= Game
+    success_url = "/juego/list"
+    fields = ["titulo", "descripcion", "genero", "fecha_de_estreno", "tipo_de_juego", "caratula"]
+
+
+class EliminarJuego(LoginRequiredMixin, DeleteView):
+
+    model= Game
+    success_url = "/juego/list"
+
+
